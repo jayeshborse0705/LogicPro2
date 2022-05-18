e39b6637b950d7063e5ee0226362a1409d6ffa33
jayeshborse0705 <jayeshborse0705@gmail.com>
2022-05-18 19:34:57 +0530

    [jayesh]  To check Perfect Number

---
+++ Logical123/PerfectNo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicalProgram
+{
+    internal class PerfectNum
+    {
+        public void Perfect_Num()
+        {
+            Console.WriteLine("Enter your number to check it's perfect number or not");
+            int Num = Convert.ToInt32(Console.ReadLine());
+            int  Sum = 0;
+            if (Num <= 0)
+            {
+                Console.WriteLine("Please enter a correct number");
+            }
+            else if (Num == 1)
+            {
+                Console.WriteLine("{0} is not a perfect number", Num);
+            }
+            else
+            {
+                for (int i = 1; i <= Num / 2; i++)
+                {
+                    if (Num % i == 0)
+                    {
+                        Sum = Sum + i;
+
+                    }
+                }
+
+                if (Sum == Num)
+                {
+                    Console.WriteLine("Yes this number {0} is a perfect number", Num);
+                }
+                else
+                {
+                    Console.WriteLine("No this number {0} is not a perfect number", Num);
+                }
+
+            }
+        }
+    }
+}
--- Logical123/Program.cs
+++ Logical123/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 
+using System;
+
 namespace LogicalProgram
 {
     internal class Program
@@ -7,8 +9,26 @@ namespace LogicalProgram
         static void Main(string[] args)
         {
 
-            Fibonacci FiboNacci = new Fibonacci();
-            FiboNacci.FibonacciSeries();
+            Console.WriteLine("Please enter number as per options");
+            Console.WriteLine("1: For Flip a coin and find % of Head and Tail");
+            Console.WriteLine("2: For checking leap year");
+            int check = Convert.ToInt32(Console.ReadLine());
+            switch (check)
+            {
+
+                case 1:
+                    Fibonacci fibonacci = new Fibonacci();
+                    fibonacci.FibonacciSeries();
+                    break;
+                case 2:
+                    PerfectNum perfectNum = new PerfectNum();
+                    perfectNum.Perfect_Num();
+                    break;
+                default:
+                    Console.WriteLine("Please SELECT correct number ! ");
+                    break;
+
+            }
         }
     }
 }
